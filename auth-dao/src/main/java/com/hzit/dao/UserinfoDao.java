9b111169a235b22d9cf81142b9ff704f489cf857
THINK <h15879020994@163.com>
2017-08-04 11:23:58 +0800

    配置

--- auth-dao/src/main/java/com/hzit/dao/UserinfoDao.java
+++ auth-dao/src/main/java/com/hzit/dao/UserinfoDao.java
@@ -1,10 +1,23 @@
 package com.hzit.dao;
 
+import com.hzit.entity.Userinfo;
+
+import java.util.List;
+
 /**
  * Created by THINK on 2017/8/3.
  */
 public interface UserinfoDao {
+    public List<Userinfo> findUser();
 
+    public int addUser(Userinfo userinfo);
 
+    public int updateUser(Userinfo userinfo);
+
+    public int deleteUser(String uid);
+
+    public Userinfo findUserByNameAndPwd(String name,String pwd);
 }
+
+
 
