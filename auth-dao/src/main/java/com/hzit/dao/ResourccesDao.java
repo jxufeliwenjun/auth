df9fe0c678171ca14b43b088e326eae7d7a76922
THINK <h15879020994@163.com>
2017-08-04 09:34:09 +0800

    配置

--- auth-dao/src/main/java/com/hzit/dao/ResourccesDao.java
+++ auth-dao/src/main/java/com/hzit/dao/ResourccesDao.java
@@ -8,6 +8,6 @@ import java.util.List;
  * Created by THINK on 2017/8/3.
  */
 public interface ResourccesDao {
-    public List<Resourcces> findResourcces(int userid);
+    public List<Resourcces> findAll();
 
 }
