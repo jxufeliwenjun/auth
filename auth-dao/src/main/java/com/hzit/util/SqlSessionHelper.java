24cd0689a6bf73364b7fad8553ae0a120150b4e6
THINK <h15879020994@163.com>
2017-08-04 08:41:28 +0800

    配置

--- auth-dao/src/main/java/com/hzit/util/SqlSessionHelper.java
+++ auth-dao/src/main/java/com/hzit/util/SqlSessionHelper.java
@@ -1,23 +1,24 @@
 package com.hzit.util;
 
-import java.io.IOException;
-import java.io.Reader;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public class SqlSessionHelper {
 	private static SqlSession session;
 	private  static SqlSessionFactory factory;
+	static org.apache.log4j.Logger log= org.apache.log4j.Logger.getLogger("SqlSessionHelper");
 	static{
 		Reader r;
 		try {
-			r = Resources.getResourceAsReader("mybatis-config.xml");
-			System.out.println("读取配置文件成功");
+			r = Resources.getResourceAsReader("mybatis");
+			log.debug("读取配置文件成功");
 			 factory=new SqlSessionFactoryBuilder().build(r);
-			System.out.println("Sqlsession工厂创建成功");
+			log.debug("Sqlsession工厂创建成功");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -25,9 +26,10 @@ public class SqlSessionHelper {
 	
 	}
 
-	public static SqlSession getSqlSession(){	
+	public static SqlSession getSqlSession(){
+		if (session==null)
 			session=factory.openSession();
-			System.out.println("获取了session对象");
+			log.debug("获取了session对象");
 			return session;
 	}
 
